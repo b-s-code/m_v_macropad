204ac4a6fd220af488176043036a535ac3326642
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-12-05 21:59:28 +0800

    layout

--- m_v_macropad/keymaps/default/keymap.c
+++ m_v_macropad/keymaps/default/keymap.c
@@ -6,19 +6,13 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
+     * │ a │ b │ c │ d │
      * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
+     * │ e │ f │ g │ h │
      * └───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    [0] = LAYOUT_ortho_2x4(
+        KC_a,   KC_b,   KC_c,   KC_d,
+        KC_e,   KC_f,   KC_g,   KC_h,
     )
 };
